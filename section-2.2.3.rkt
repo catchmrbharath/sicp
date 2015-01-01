bb142458218b264ff2ef34d45cd58a7f2ebc508d
Bharath M R <catchmrbharath@gmail.com>
2015-01-01 23:37:11 +0530

    Exercise till 2.37

--- section-2.2.3.rkt
+++ section-2.2.3.rkt
@@ -66,3 +66,92 @@
 
 (define s (list (list 1 2 3) (list 4 5 6) (list 7 8 9)))
 (accumulate-n + 0 s)
+
+;; Exercise 2.37
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (y) (dot-product v y)) m))
+
+(define mat (list (list 1 2 3) (list 4 5 6) (list 6 7 8)))
+(define v (list 1 2 3 4))
+
+(define (transpose mat)
+  (accumulate-n cons nil mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (x) (matrix-*-vector cols x)) m)))
+
+(define mat2 (list (list 1 2) (list 3 4)))
+
+
+;;Exercise 2.38
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+      result
+      (iter (op result (car rest))
+            (cdr rest))))
+  (iter initial sequence))
+
+(define fold-right accumulate)
+
+(fold-right / 1 (list 1 2 3))
+(fold-left / 1 (list 1 2 3))
+(display ( fold-right list nil (list 1 2 3)))
+(display ( fold-left list nil (list 1 2 3)))
+
+;;Exercise 2.39
+(define (reverse sequence)
+  (fold-right (lambda (x y) (append y (list x))) nil sequence))
+
+(define (reverse sequence)
+  (fold-left (lambda (x y) (cons y x)) nil sequence)
+  )
+
+(reverse (list 1 2 3))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (square x) (* x x))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+    (cons low (enumerate-interval (+ low 1) high))))
+
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (flatmap
+                (lambda (i)
+                  (map (lambda (j)  (list i j))
+                       (enumerate-interval 1 (- i 1))))
+                (enumerate-interval 1 n)))))
+
